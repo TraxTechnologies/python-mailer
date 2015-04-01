536674175b88a2eba967c773d75c2e6229218602
ryan <none@none>
2015-03-31 21:43:02 -0700

    updated setup, reformatted

--- setup.py
+++ setup.py
@@ -2,19 +2,20 @@ from setuptools import setup
 import sys
 
 extra = {}
-if sys.version_info >= (3,0):
-    extra.update( use_2to3 = True )
+if sys.version_info >= (3, 0):
+    extra.update(use_2to3=True)
 
-setup (
-    name = 'mailer',
-    version = "0.8.0",
-    description = "A module to send email simply in Python",
-    author = "Ryan Ginstrom",
-    url = "https://bitbucket.org/ginstrom/mailer",
-    py_modules = ["mailer"],
-    keywords = ["email", "smtp"],
-    license = "MIT License",
-    classifiers = [
+setup(
+    name='mailer',
+    version="0.8.0",
+    description="A module to send email simply in Python",
+    author="Ryan Ginstrom",
+    author_email='ryan@ginstrom.com',
+    url="https://bitbucket.org/ginstrom/mailer",
+    py_modules=["mailer"],
+    keywords=["email", "smtp"],
+    license="MIT License",
+    classifiers=[
         'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: MIT License',
         "Operating System :: MacOS :: MacOS X",
